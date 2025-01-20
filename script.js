f30f7c1fbc5509e5843099ec0c4fea4c1badcc39
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-20 21:45:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let element = document.getElementById("level");
+
+let level = 0; 
+while (element.parentElement) { 
+	level++;
+    element = element.parentElement;
+}
+
+alert(`The level of the element is: ${level+1}`)
